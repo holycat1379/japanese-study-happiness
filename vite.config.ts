acaf950dfe8a60d3ecf0802a385419b6bebf7afb
holycat <holycat1379@outlook.com>
2024-04-02 10:35:46 +0800

    test3444555224444555555

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'node:path'
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  assetsInclude: ['**/*.vtt', '**/*.mp3', '**/*.mp4', '**/*.json', '**/*.gz'],
+  plugins: [nodePolyfills(),vue(),{
+    name: 'json-to-js',
+    transform(src, id) {
+      if (id.endsWith('.json')) {
+        return {
+          code: `export default ${src}`,
+          map: null
+        };
+      }
+    }
+  }],
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  base: '/japanese-study-happiness/',
+  base: '/japanese-study-happiness/'
 })
